6a97c73b71cb496378fb0eee8562256c29325fe3
Justin Fulton <jusfulton@gmail.com>
2020-11-13 12:27:45 -0800

    Initials complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
-	foo = () => "Bars";
 	render() {
-		const name = "Jimmy Joe";
-		const loading = false;
-		const showName = true;
-
-		// const foo = () => "Bar";
 		return (
 			<div className='App'>
-				<h1>My Appz</h1>
-				{loading ? <h4>Loading Folz</h4> : <h4>Hello {showName && name}!</h4>}
+				<Navbar />
 			</div>
 		);
 	}
